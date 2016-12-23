bf8e353d3555d9a360d8955d9023c0e4a2e657eb
SandChow <srisandeep@hotmail.com>
2016-12-23 15:08:39 -0500

    add reddit-reader implemented in python

---
+++ reddit-reader/reddit_reader.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_ask import Ask, statement, question, session
+import json
+import time
+import requests
+import unidecode
+
+app = Flask(__name__)
+ask = Ask(app, '/reddit_reader')
+
+def get_headlines():
+    pass
+
+@app.route('/')
+def homepage():
+    pass
+
+@ask.launch
+def start_skill():
+    welcome_message = "Hey there, would you like the headlines of the day?"
+    return question(welcome_message)
+
+@ask.intent("YesIntent")
+def share_headlines():
+    headlines = get_headlines()
+    headline_msg = "The current world news headlines are {}".format(headlines)
+    return statement(headline_msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
